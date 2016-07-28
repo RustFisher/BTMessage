e5d2ea1bbcf25825f5cd0d319c28f206d374fc0e
RustFisher <rustfisher@foxmail.com>
2016-07-28 14:08:32 +0800

    delete log

--- app/src/main/java/com/example/android/BluetoothChat/BluetoothChatActivity.java
+++ app/src/main/java/com/example/android/BluetoothChat/BluetoothChatActivity.java
@@ -146,7 +146,6 @@ public class BluetoothChatActivity extends AppCompatActivity {
     }
 
     private void ensureDiscoverable() {
-        if (D) Log.d(TAG, "ensure discoverable");
         if (mBluetoothAdapter.getScanMode() !=
                 BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
             Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
